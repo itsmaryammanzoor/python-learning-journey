779c7979f50676daf6fd74068632289bc424d454
Maryam Manzoor <itsmaryammanzoor@gmail.com>
2026-08-26 20:52:47 +0500

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,50 @@
+# print("list of 5 cities")
+
+# cities = ["depalpur","okara","sahiwal","lahore","sialkot"]
+# print(cities[2])     #print city at position 3
+# cities.append("multan")   #Add one new city
+# print(cities)
+# cities.insert(1,"renala")   #add new city at position 2
+# print(cities)
+# cities.remove("sialkot")    #remove any city 
+# print(cities)
+# cities.sort()     # sort the list alphabatically
+# print(cities)
+# print(len(cities))      #print length of list
+
+
+
+# print("number list with for loop and their sum")
+
+# num = [10 , 20 , 30 , 40 , 50 ]
+# total=0
+# for number in num:
+#     print(number)
+#     total += number
+# print("total= ",total)
+
+
+#print("Find the highest mark using a for loop.")
+# high = 0
+# marks = [65, 82, 45, 91, 73]
+# for mark in marks:
+#     if mark >= high:
+#         high = mark
+# print("highest mark is ", high)
+
+
+# print("Find the lowest mark without using min().")
+
+# total = 0
+# marks = [65, 82, 45, 91, 73]
+# low = marks[0]
+# for mark in marks:
+#     if mark <= low:
+#         low = mark
+#     total += mark
+# avrg = total /len(marks)  
+# print("lowest mark is ",low)
+# print ("average is ",avrg)
+
+
+
