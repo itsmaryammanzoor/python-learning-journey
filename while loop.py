cecdfbc91e7c863dbe2c5b52f9e034d143d3ed29
Maryam Manzoor <itsmaryammanzoor@gmail.com>
2026-08-26 20:53:09 +0500

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,51 @@
+# i = 1
+
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+#print("ask uer to entr password ")
+
+# password = input("Enter the password: ")
+# while password != "python123":
+#       password = input("Wrong password.Try again!")
+# print("Login Successful!")
+
+
+#print("ask uder to guess a secret number ")
+
+# secret =int(input (" guess the number"))
+# while secret != 7:
+#       secret = int (input ("wrong guess.try again: "))
+# print("Correct! You guessed it!")
+
+
+#print("1 to 10 counting and their sum")
+
+# j=0
+# i = 10
+# while i >= 2 :
+#     print(i)
+#     j+=i
+#     i -=2
+# print(j)
+  
+
+#print("table of given number")
+
+# j = int ( input("enter a number"))
+# i = 0
+# while i <= 10 :
+#     print(j,"*",i,"=",i*j)
+#     i += 1
+
+
+# print("factorial of given number" )
+
+# num = int(input("Enter a number: "))
+# i = num
+# factorial = 1
+# while i >= 1:
+#      factorial*=i
+#      i-=1
+# print(factorial)
