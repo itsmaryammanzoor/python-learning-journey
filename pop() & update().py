42d29f65550fe8695072c22b00d0a7884ed2f7fd
Maryam Manzoor <itsmaryammanzoor@gmail.com>
2026-08-31 09:06:12 +0500

    Add files via upload

---
+++ pop() & update().py
@@ -0,0 +1,24 @@
+# student = {
+#     "Name" : "Maryam",
+#     "Age" : 21,
+#     "City" : "Depalpur",
+#     "Marks" : 89
+# }
+# student.pop("Age")
+# student.update({"status" : "passed",
+# "Age " : 22,
+# "city": "Lahore"
+#                 })
+
+# for key, value in student.items():
+#    print(key,"=",value)
+
+print("clear()  operation")
+dic={
+    "name": "ali",
+    "age": 24,
+    "marks": 90,
+}
+print(dic)
+dic.clear()
+print(dic)
